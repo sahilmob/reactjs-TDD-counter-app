8e80a8884dfc850a6575f8ef0c0ba3d7e280446d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 12:06:38 +0300

    setup project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+
+function App () {
+	let [counter, setCounter] = useState(0)
+	return (
+		<div className="App">
+			<h1>The count is {counter}</h1>
+			<button onClick={() => setCounter(++counter)}>Increment Counter</button>
+		</div>
+	);
 }
 
 export default App;
