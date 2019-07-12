16f4501fa80d8b027f1e37ffd263dc17daa46ceb
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 12:47:40 +0300

    add data-test attr. to the parent div

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 function App () {
 	let [counter, setCounter] = useState(0)
 	return (
-		<div className="App">
+		<div className="App" data-test="component-app">
 			<h1>The count is {counter}</h1>
 			<button onClick={() => setCounter(++counter)}>Increment Counter</button>
 		</div>
