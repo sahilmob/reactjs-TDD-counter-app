d36a0631a40ec6864fd107b16b9836fac3af47e7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 12:52:36 +0300

    add data-test attr. to counter display and increment button

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App () {
 	let [counter, setCounter] = useState(0)
 	return (
 		<div className="App" data-test="component-app">
-			<h1>The count is {counter}</h1>
-			<button onClick={() => setCounter(++counter)}>Increment Counter</button>
+			<h1 data-test="counter-display">The count is {counter}</h1>
+			<button data-test="increment-button" onClick={() => setCounter(++counter)}>Increment Counter</button>
 		</div>
 	);
 }
